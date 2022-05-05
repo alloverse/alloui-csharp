d97a88c52602e1b6ef641237305afeab5e133850
Patrik Sjöberg <voxxar@gmail.com>
2022-05-05 15:29:25 +0200

    relative liballonet path

--- AlloUI/Allonet/_Interfaces.cs
+++ AlloUI/Allonet/_Interfaces.cs
@@ -8,7 +8,7 @@ namespace AlloUI
     [StructLayout(LayoutKind.Sequential)]
     struct _AlloClient
     {
-        const string _dllLocation = "/home/nevyn/Dev/allonet/build/liballonet.so";
+        const string _dllLocation = "lib/liballonet";
         [DllImport(_dllLocation)]
         public unsafe static extern bool allo_initialize(bool redirect_stdout);
 
--- README.md
+++ README.md
@@ -7,3 +7,8 @@ for Alloverse using a high-level and easy to use UI library
 that should feel familar to people coming from iOS, Android
 or web development, rather than a game development background.
 
+
+
+## Setup
+
+Place liballonet.xx in `<ProjectPath>/lib/`
